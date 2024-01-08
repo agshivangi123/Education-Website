7ec3363e652644d9e095431c261064b6bb6a6700
agshivangi123 <133387158+agshivangi123@users.noreply.github.com>
2024-01-08 20:43:19 +0530

    Add files via upload

---
+++ resp.js
@@ -0,0 +1,10 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+rightNav = document.querySelector('.rightNav');
+
+burger.addEventListener('click' , ()=>{
+    rightNav.classList.toggle('v-nav')
+    navlist.classList.toggle('v-nav')
+    navbar.classList.toggle('h-nav')
+});
